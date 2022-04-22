153267c6c0f7d9c76426117a898555dc321fd48e
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2022-04-22 18:18:16 -0300

    Add files via upload

--- cadastrar_perguntas.php
+++ cadastrar_perguntas.php
@@ -1,8 +1,20 @@
-<?php
-$pergunta = $_POST['pergunta'];
-$resposta1 = $_POST['resposta1'];
-$resposta2 = $_POST['resposta2'];
-$resposta3 = $_POST['resposta3'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questões</title>
+</head>
+<body>
+    <?php
 
+    echo $_POST['pergunta'];
+    echo $_POST['resposta1'];
+    echo $_POST['resposta2'];
+    echo $_POST['resposta3'];
+    
+    ?>
+</body>
+</html>
 
-?>
